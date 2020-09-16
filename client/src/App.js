7665071b0dc8be76f329c2f9e359e95494747e23
domi <domi@appstablishment.com>
2020-09-16 11:54:27 +0800

    Add 404, errorHandler, change files to be jsx

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import Customer from './pages/customer';
 import Home from './pages/home';
+import ErrorHandler from './components/errorHandler';
+
 
 class App extends Component {
   render() {
     return (
         <BrowserRouter>
-          {/* <ErrorHandler> */}
+          <ErrorHandler> 
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/customer/:customerId/" component={Customer} />
               {/* <Route component={Page404} /> */}
             </Switch>
-          {/* </ErrorHandler> */}
+          </ErrorHandler>
         </BrowserRouter>
     );
   }
